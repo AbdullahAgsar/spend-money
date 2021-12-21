1df82f5e08512119f680e4d5a39a468fcc8285c6
AbdullahAgsar <agsar7634@gmail.com>
2021-12-21 03:21:27 +0300

    Products components and styles added

--- src/components/Products.js
+++ src/components/Products.js
@@ -3,7 +3,11 @@ import '../styles/Products.css'
 function Products (props) {
     return(
         <div className="Product">
-            
+            <img src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg"></img>
+
+            <div className="Title">efsfse</div>
+
+            <div className="Price">1515</div>
         </div>
     )
 }
